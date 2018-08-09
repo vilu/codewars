742950d28469ee11eda17b36aa6ffb865435afa3
Viktor Lund <vlund@ebay.com>
2018-08-09 10:14:09 +0200

    Tests passing MultNumAsStrings

--- src/Kata/MultNumAsStrings.hs
+++ src/Kata/MultNumAsStrings.hs
@@ -4,4 +4,7 @@ import           Data.Char (digitToInt, intToDigit)
 
 -- | mulitply two numbers as strings
 multiply :: String -> String -> String
-multiply xs ys = undefined
+multiply xs ys = show $ x * y
+                where
+                    x = (read xs) :: Int
+                    y = (read ys) :: Int
