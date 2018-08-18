cc8d6b5ca755c50887ed7f6ac98487ac6752eb33
Viktor Lund <vlund@ebay.com>
2018-08-17 17:13:45 -0700

    Naive solution to Calculator kata

--- src/Kata/Calculator.hs
+++ src/Kata/Calculator.hs
@@ -1,4 +1,35 @@
 module Kata.Calculator where
 
+data Expression = Division | Multiplication | Addition | Subtraction | Val Double deriving (Show)
+
+
 evaluate :: String -> Double
-evaluate = undefined
+evaluate = val . secondPass . firstPass . readExp
+
+readExp :: String -> [Expression]
+readExp s = fmap parseExp $ words s
+            where
+                parseExp "*" = Multiplication
+                parseExp "/" = Division
+                parseExp "+" = Addition
+                parseExp "-" = Subtraction
+                parseExp xs  = Val (read xs :: Double)
+
+firstPass :: [Expression] -> [Expression]
+firstPass xs = reverse $ foldl go [] xs
+                where
+                    go :: [Expression] -> Expression -> [Expression]
+                    go acc'@(Multiplication:(Val x):acc) (Val y) = (Val (x*y)):acc
+                    go acc'@(Division:(Val x):acc)       (Val y) = (Val (x/y)):acc
+                    go  acc                              e        = e:acc
+
+secondPass :: [Expression] -> [Expression]
+secondPass xs = reverse $ foldl go [] xs
+                where
+                    go :: [Expression] -> Expression -> [Expression]
+                    go  (Addition:(Val x):acc)     (Val y) = (Val (x+y)):acc
+                    go  (Subtraction:(Val x):acc)  (Val y) = (Val (x-y)):acc
+                    go  acc                        e       = e:acc
+
+val :: [Expression] -> Double
+val ((Val d):ds) = d
--- test/Kata/CalculatorSpec.hs
+++ test/Kata/CalculatorSpec.hs
@@ -7,12 +7,8 @@ import           Test.QuickCheck
 main :: IO ()
 main = hspec $ spec
 
-spec = describe "DISABLED" $ do
-  it "ignore" $ do
-    True `shouldBe` True
-
--- spec :: Spec
--- spec = describe "simple expressions" $ do
---     it "should_work_for_some_simple_expressions" $ do
---         evaluate "2 / 2 + 3 * 4 - 13" `shouldBe` 0
---         evaluate "4 + 3 * 4 / 3 - 6 / 3 * 3 + 8"  `shouldBe` 10
+spec :: Spec
+spec = describe "simple expressions" $ do
+    it "should_work_for_some_simple_expressions" $ do
+        evaluate "2 / 2 + 3 * 4 - 13" `shouldBe` 0
+        evaluate "4 + 3 * 4 / 3 - 6 / 3 * 3 + 8"  `shouldBe` 10
