98322badbe1f2e6ce58095ad413a161635e42640
Viktor Lund <vlund@ebay.com>
2018-09-08 09:25:24 +0200

    Huffman encoding kata

--- src/Kata/Huffman.hs
+++ src/Kata/Huffman.hs
@@ -1,22 +1,85 @@
-module Kata.Huffman
-    ( frequencies
+module Kata.Huffman ( 
+      frequencies
     , encode
     , decode
     , Bit (..)
     ) where
 
 import           Data.List
+import           Data.Maybe
+import           Control.Monad
 
 data Bit = Z | O deriving (Eq, Show)
 
--- | Calculate symbol frequencies of a text.
-frequencies :: Ord a => [a] -> [(a, Int)]
-frequencies = error "frequencies not yet implemented"
+data Tree a = Leaf (a, Int)  | Node (Tree a) Int (Tree a) deriving (Eq, Show)
+
+weight :: Tree a -> Int
+weight (Node l w r) = w
+weight (Leaf (_, w)) = w
 
 -- | Encode a sequence using the given frequencies.
 encode :: Ord a => [(a, Int)] -> [a] -> Maybe [Bit]
-encode = error "encode not yet implemented"
+encode freqs as| length freqs < 2 = Nothing
+               | otherwise = fmap join $ sequence bits
+                        where 
+                            bits :: [Maybe [Bit]]
+                            bits = fmap (\a -> (aToBits freqs >>= (\aTB -> findBitsForA aTB a))) as
+
+                            findBitsForA :: Ord a => [(a, [Bit])] -> a -> Maybe [Bit]
+                            findBitsForA aToBit a  = fmap (snd) $ find (\(a', _) -> a == a') aToBit
+
+                            aToBits :: [(a, Int)] -> Maybe [(a, [Bit])]
+                            aToBits freqs = fmap treeToBits $ constructTree freqs
+
+                            treeToBits :: Tree a -> [(a, [Bit])]
+                            treeToBits t = recurse [] t
+                                                where
+                                                    recurse pos (Leaf (a, _)) = [(a, (reverse pos))]
+                                                    recurse pos (Node l _ r) = (recurse (Z:pos) l) ++ (recurse (O:pos) r)
+
+
 
 -- | Decode a bit sequence using the given frequencies.
-decode :: [(a, Int)] -> [Bit] -> Maybe [a]
-decode = error "decode not yet implemented"
+decode :: (Ord a, Show a) => [(a, Int)] -> [Bit] -> Maybe [a]
+decode freqs bits| length freqs < 2 = Nothing 
+                 | length bits == 0 = Just []
+                 | otherwise = rootTree >>= (\root -> decodeRec root root  bits (Just []))
+                                    where 
+                                        rootTree = constructTree freqs 
+
+                                        decodeRec :: (Ord a, Show a) => Tree a -> Tree a -> [Bit] -> Maybe [a] -> Maybe [a]
+                                        decodeRec root (Leaf (a,_)) [] (Just acc) = Just (reverse $ (a:acc))
+                                        decodeRec root (Leaf (a,_)) bits (Just acc) = decodeRec root root bits (Just (a:acc))
+                                        decodeRec root (Node l _ r) (b:bits) acc = decodeRec root (if b == Z then l else r) bits acc
+
+
+frequencies :: Ord a => [a] -> [(a, Int)]
+frequencies = sortOnSnd . (foldl freq [])
+                where
+                    freq ((a,n):as) b|a == b = (a,n+1):as
+                    freq as a         = (a,1):as
+
+                    sortOnSnd :: Ord b => [(a, b)] -> [(a, b)]
+                    sortOnSnd = (sortOn snd)
+
+
+constructTree :: [(a, Int)] -> Maybe (Tree a)
+constructTree = mergeLeafs . mkLeafs
+                    where 
+                        mkLeafs :: [(a, Int)] -> [Tree a]
+                        mkLeafs = fmap Leaf
+
+                        merge :: Tree a -> Tree a -> Tree a
+                        merge l r = Node l ((weight l) + (weight r)) r
+
+                        mergeLeafs :: [Tree a] -> Maybe (Tree a)
+                        mergeLeafs ts = if length ts < 2 then Nothing else go ts
+                                where 
+                                    go ts = case (sortOn weight ts) of [] -> Nothing
+                                                                       t1:t2:ts -> go $ (merge t1 t2):ts
+                                                                       t:[] -> Just t
+
+
+
+-- -- test :: [(Char, Int)]
+-- test = (aToBits . frequencies) "aaaaabbccccdeeeeeeeefff"
--- test/Kata/HuffmanSpec.hs
+++ test/Kata/HuffmanSpec.hs
@@ -6,33 +6,30 @@ import           Test.Hspec
 -- Codewars wraps HSpec: http://hspec.github.io/
 main = hspec $ spec
 
-spec = describe "DISABLED" $ do
-  it "ignore" $ do
-    True `shouldBe` True
--- spec = do
---     describe "basic tests" $ let fs = frequencies "aaaabcc" in do
---         it "aaaabcc encoded should have length 10" $
---             fmap length (encode fs "aaaabcc") `shouldBe` Just 10
---         it "empty list encode" $
---             encode fs [] `shouldBe` Just []
---         it "empty list decode" $
---             decode fs [] `shouldBe` Just []
+spec = do
+    describe "basic tests" $ let fs = frequencies "aaaabcc" in do
+        it "aaaabcc encoded should have length 10" $
+            fmap length (encode fs "aaaabcc") `shouldBe` Just 10
+        it "empty list encode" $
+            encode fs [] `shouldBe` Just []
+        it "empty list decode" $
+            decode fs [] `shouldBe` Just []
 
---     describe "length" $ do
---         it "equal lengths with same frequencies if alphabet size is a power of two" $
---             let enc = encode [('a', 1), ('b', 1)]
---             in mapM (fmap length) [enc "a", enc "b"] `shouldBe` Just [1, 1]
---         it "smaller length for higher frequency, if size of alphabet is not power of two" $
---             let enc = encode [('a', 1), ('b', 1), ('c', 2)]
---             in mapM (fmap length) [enc "a", enc "b", enc "c"] `shouldBe` Just [2, 2, 1]
+    describe "length" $ do
+        it "equal lengths with same frequencies if alphabet size is a power of two" $
+            let enc = encode [('a', 1), ('b', 1)]
+            in mapM (fmap length) [enc "a", enc "b"] `shouldBe` Just [1, 1]
+        it "smaller length for higher frequency, if size of alphabet is not power of two" $
+            let enc = encode [('a', 1), ('b', 1), ('c', 2)]
+            in mapM (fmap length) [enc "a", enc "b", enc "c"] `shouldBe` Just [2, 2, 1]
 
---     describe "error handling" $ do
---         it "empty frequencies encode 1" $ encode [] "abc" `shouldBe` Nothing
---         it "empty frequencies encode 2" $ encode [] "" `shouldBe` Nothing
---         it "singleton frequency encode 1" $ encode [('a', 1)] "a" `shouldBe` Nothing
---         it "singleton frequency encode 2" $ encode [('a', 1)] "" `shouldBe` Nothing
+    describe "error handling" $ do
+        it "empty frequencies encode 1" $ encode [] "abc" `shouldBe` Nothing
+        it "empty frequencies encode 2" $ encode [] "" `shouldBe` Nothing
+        it "singleton frequency encode 1" $ encode [('a', 1)] "a" `shouldBe` Nothing
+        it "singleton frequency encode 2" $ encode [('a', 1)] "" `shouldBe` Nothing
 
---         it "empty frequencies decode 1" $ (decode [] [Z, O] :: Maybe String) `shouldBe` Nothing
---         it "empty frequencies decode 2" $ (decode [] [] :: Maybe String) `shouldBe` Nothing
---         it "singleton frequency decode 1" $ decode [('a', 1)] [Z, O] `shouldBe` Nothing
---         it "singleton frequency decode 2" $ decode [('a', 1)] [] `shouldBe` Nothing
+        it "empty frequencies decode 1" $ (decode [] [Z, O] :: Maybe String) `shouldBe` Nothing
+        it "empty frequencies decode 2" $ (decode [] [] :: Maybe String) `shouldBe` Nothing
+        it "singleton frequency decode 1" $ decode [('a', 1)] [Z, O] `shouldBe` Nothing
+        it "singleton frequency decode 2" $ decode [('a', 1)] [] `shouldBe` Nothing
